f39cee6fe874412e2f2f77f149f53fedecdc5870
Benjamin Eze <ezebenjaminch@gmail.com>
2024-07-01 01:14:34 +0000

    getting user's location

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const app = require("./app")
+const ipInfo = require("ip-info-finder");
+
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     const ipAddress = req.ip
-    res.send(`Hello World! ${ipAddress}`)
+    
+
+    const locationInfo = ipInfo.getIPInfo(ipAddress).then(data => {
+     
+})
+.catch(err => console.log(err));
+    res.send(`Hello World! ${ipAddress} ${locationInfo}`)
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
