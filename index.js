33f7b456cd16efd0c2c614c40f199edfcfcb8f27
Benjamin Eze <ezebenjaminch@gmail.com>
2024-07-01 01:44:51 +0000

    test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const app = require("./app")
-const ipInfo = require("ip-info-finder");
+const getGeoData = require("@sasmeee/ip-locator");
 
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     const ipAddress = req.ip
+    const data = await getGeoData(ipAddress);
+console.log(data);
     
 
-ipInfo.getIPInfo(ipAddress).then(data => {
-    const locationInfo = data
-    res.send(`Hello World! ${ipAddress} ${locationInfo}`)
-})
-.catch(err => console.log(err));
+
+    res.send(`Hello World! ${ipAddress} ${data}`)
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
