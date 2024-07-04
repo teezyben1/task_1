12a203ae2c68549a4f100c609fb9961f15126246
Benjamin Eze <ezebenjaminch@gmail.com>
2024-07-04 20:55:44 +0000

    testing

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const unirest = require('unirest')
+
+const app = express();
+
+const port = 3000;
 
 
 
+app.get("/", (req, res) => {
 
-app.set('trust proxy', true);
+  var apiCall = unirest("GET",
+
+    "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+
+  );
+
+  apiCall.headers({
+
+    "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+
+    "x-rapidapi-key": "d42e93f52dmshc8bba125c499e43p17f7cbjsn6fbe4c59b01e"
+
+  });
+
+  apiCall.end(function(result) {
+
+    if (res.error) throw new Error(result.error);
+
+    console.log(result.body);
+
+    res.send(result.body);
+
+  });
+
+});
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    
+
+
 
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const app = require("./app")
-
 const port = process.env.PORT || 8080
 
-app.get('/', async (req, res) => {
-    const ipAddress = req.ip
-    
-
-
-    res.send(`Hello World! ${ipAddress} ${data}`)
-})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
